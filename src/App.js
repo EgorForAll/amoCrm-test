6cc43cb527787273b454fdfb91292d364ac09571
EgorForAll <eia.web.ss@gmail.com>
2024-03-04 12:31:25 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import {ACCESS_TOKEN} from "./const";
 
 function App() {
+  const [leads, setLeads] = useState()
+  useEffect(() => {
+    fetch('/api/v4/leads', {headers: {
+        "Authorization": `Bearer ${ACCESS_TOKEN}`,
+        "Access-Control-Allow-Headers": '*'
+      }}).then((res) => res.json()).then((res) => setLeads(res._embedded.leads))
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>This is amoCRM test</h1>
     </div>
   );
 }
