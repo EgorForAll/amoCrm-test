9c734785a812eb739eaf666dbaecc136bd97f49b
EgorForAll <eia.web.ss@gmail.com>
2024-03-05 14:00:40 +0300

    api functions created

---
+++ src/api/fetch-all.js
@@ -0,0 +1,11 @@
+import {ACCESS_TOKEN} from "../const/const";
+
+
+export const fetchAll = async (hook) =>
+    await fetch('/api/v4/leads', {
+        headers: {
+            "Authorization": `Bearer ${ACCESS_TOKEN}`,
+            "Access-Control-Allow-Headers": '*'
+        }
+    }).then((res) => res.json()).then((res) => hook(res._embedded.leads)).catch((e) => console.error(e))
+
